20166591b9d64bb3d037134897c75255b42ddc12
Ayush1Sikarwar <ayushlegend1212@gmail.com>
2025-08-08 10:45:42 +0530

    Substraction

--- calculator.java
+++ calculator.java
@@ -7,10 +7,15 @@ class Calculator {
         int a = sc.nextInt();
         int b = sc.nextInt();
         System.out.println("Addition: " + add(a, b));
+        System.out.println("Subtraction: " + subtract(a, b));
     }
 
     public static int add(int x, int y) {
         return x + y;
+    }
+
+    public static int subtract(int x, int y) {
+        return x - y;
     }
 
 }
